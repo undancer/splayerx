1ddd6ba07989b8255c7350e858f18f9976181c11
undancer <>
2022-07-23 19:25:48 +0800

    使能

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -1,17 +1,19 @@
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 const { exec } = require('child_process');
 
-require('events').EventEmitter.prototype._maxListeners = 10000;
+// require('events').EventEmitter.prototype._maxListeners = 10000;
 
 const commands = [
+  'patch-package',
   'node scripts/gen-electron-builder-config.js',
   // 'npm run lint:fix',
   'npm run lint',
   'npm run install-app-deps',
 ];
 
-exec(commands.join('&&'), (error, stdout) => {
+exec(commands.join(' && '), (error, stdout) => {
   // if (error) throw error;
+  // eslint-disable-next-line no-console
   console.log(error, stdout);
 });
